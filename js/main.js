4890676c46dc6f7986eb70dfe88663afdf4e6a23
Bishal-9 <goonbishal.bg@gmail.com>
2021-11-17 14:15:34 +0530

    Game Logic

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@ scene.add(light);
 // Global Variables
 const start_position = 3;
 const end_position = -start_position;
+const text = document.querySelector('.text');
+const TIME_LIMIT = 10;
+let gameStat = 'loading';
+let isLookingBackward = true;
 
 function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
     const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
@@ -44,10 +48,12 @@ class Doll {
 
     lookBackward() {
         gsap.to(this.doll.rotation, { y: -3.15, duration: 0.45 });
+        setTimeout(() => isLookingBackward = true, 150);
     }
 
     lookForward() {
         gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
+        setTimeout(() => isLookingBackward = false, 450);
     }
 
     async start() {
@@ -90,7 +96,20 @@ class Player {
         gsap.to(this.playerInfo, { velocity: 0, duration: 0.1 })
     }
 
+    check() {
+        if (this.playerInfo.velocity > 0 && !isLookingBackward) {
+            text.innerText = 'You Lost!!'
+            gameStat = 'over'
+        }
+
+        if (this.playerInfo.positionX < end_position + 0.4) {
+            text.innerText = 'You Won!'
+            gameStat = 'over'
+        }
+    }
+
     update() {
+        this.check();
         this.playerInfo.positionX -= this.playerInfo.velocity;
         this.player.position.x = this.playerInfo.positionX;
     }
@@ -99,11 +118,37 @@ class Player {
 const player = new Player()
     
 let doll = new Doll()
-setTimeout(() => {
+
+async function init() {
+    await delay(500);
+    text.innerText = 'Starting in 3';
+    await delay(500);
+    text.innerText = 'Starting in 2';
+    await delay(500);
+    text.innerText = 'Starting in 1';
+    await delay(500);
+    text.innerText = 'Goo!!'
+    startGame();
+}
+
+function startGame() {
+    gameStat = 'started';
+    let progressBar = createCube({ w: 5, h: 0.1, d: 1 }, 0);
+    progressBar.position.y = 3.35;
+    gsap.to(progressBar.scale, { x: 0, duration: TIME_LIMIT, ease: 'none' })
     doll.start();
-}, 1000);
+    setTimeout(() => {
+        if (gameStat !== 'over') {
+            text.innerText = 'You ran out of time!';
+            gameStat = 'over';
+        }
+    }, TIME_LIMIT * 1000);
+}
+
+init();
 
 function animate() {
+    if (gameStat === 'over') return;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     player.update();
@@ -120,6 +165,7 @@ function onWindowResize() {
 }
 
 window.addEventListener('keydown', e => {
+    if(gameStat != 'started') return
     if (e.key == 'ArrowUp') {
         player.run()
     }
