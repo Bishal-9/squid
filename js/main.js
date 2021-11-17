5d211e7b1b017e2e218d1e1db88ac8e583cff3c5
Bishal-9 <goonbishal.bg@gmail.com>
2021-11-17 13:38:41 +0530

    Key Handling

--- js/main.js
+++ js/main.js
@@ -74,6 +74,10 @@ class Player {
         this.playerInfo.velocity = 0.03;
     }
 
+    stop() {
+        gsap.to(this.playerInfo, { velocity: 0, duration: 0.1 })
+    }
+
     update() {
         this.playerInfo.positionX -= this.playerInfo.velocity;
         this.player.position.x = this.playerInfo.positionX;
@@ -102,3 +106,15 @@ function onWindowResize() {
 
     renderer.setSize(window.innerWidth, window.innerHeight)
 }
+
+window.addEventListener('keydown', e => {
+    if (e.key == 'ArrowUp') {
+        player.run()
+    }
+})
+
+window.addEventListener('keyup', e => {
+    if (e.key == 'ArrowUp') {
+        player.stop()
+    }
+})
